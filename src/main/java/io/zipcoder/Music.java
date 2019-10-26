9afa8020099ec4f5ddb60c802643f3df36fe6120
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-10-26 19:49:54 -0400

    finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,35 @@
 package io.zipcoder;
 
-public class Music {
+public class Music<startindex> {
+
+
 
     private String[] playList;
 
+
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+
+
+    public Integer selection(Integer startIndex, String selection) {
+
+        int counterForward = 0;
+        int counterBackward = 0;
+        for (int i = 0; i <= startIndex; i++) {
+            counterForward++;
+        }
+        for (int j = 0; j <= playList.length; j++) {
+            counterBackward++;
+        }
+
+        if (counterForward < counterBackward) {
+
+            return counterForward;
+        } else {
+            return counterBackward;
+        }
     }
 }
